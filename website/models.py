629248343a0447c594aeb3b26250666d01c7a5f1
Jeff Gilbert <jeff.gilbert@usanad.com>
2021-11-25 13:26:12 -0500

    Before switching to declarative_base

--- website/models.py
+++ website/models.py
@@ -1,7 +1,7 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
-
+from sqlalchemy.orm import relationship
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,3 +16,40 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+
+class Student(db.Model):
+    __tablename__ = 'students'
+    id= db.Column( db.Integer, primary_key=True)
+    instruments = relationship("StudentInstrument",back_populates="student")
+    email =  db.Column( db.String(150), unique=True)
+    first_name =  db.Column( db.String(150))
+    last_name =  db.Column( db.String(150))
+    notes =  db.Column( db.String(150))
+    birthday = db.Column( db.Date)
+
+
+class Instrument(db.Model):
+    __tablename__ = 'instruments'
+    id       = db.Column(db.Integer, primary_key=True)
+    students = relationship("StudentInstrument",back_populates="instrument")
+    tag      = db.Column(db.String(150))
+    serial   = db.Column(db.String(150))
+    type     = db.Column(db.String(150))
+    size     = db.Column(db.String(150))
+    brand    = db.Column(db.String(150))
+
+class StudentInstrument(db.Model):
+    __tablename__ = 'students_instruments'
+    student_id = db.Column(db.ForeignKey('students.id'),primary_key=True)
+    instrument_id = db.Column(db.ForeignKey('instruments.id'),primary_key=True)
+    student=relationship("Student",back_populates="instruments")
+    instrument=relationship("Instrument",back_populates="students")
+    checkout_date = db.Column(db.Date,primary_key=True)
+    checkout_condition = db.Column(db.Integer)
+    due_date = db.Column(db.Date)
+    return_date = db.Column(db.Date)
+    return_condition = db.Column(db.Integer)
+    return_location = db.Column(db.String(100))
+    notes = db.Column(db.String(1000))
+ 
--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, Student, Instrument, StudentInstrument
 from . import db
 import json
+import sqlalchemy
 
 views = Blueprint('views', __name__)
 
@@ -35,3 +36,68 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+
+# Student
+@views.route('/student/new', methods=['GET','POST'])
+@login_required
+def student_new():
+    if request.method == 'POST':
+        try:
+            student = Student(
+                email = request.form.get('email'),
+                first_name = request.form.get('firstName'),
+                last_name = request.form.get('lastName')
+                )
+            db.session.add(student)
+            db.session.commit()
+            flash('Student Added!', category='success')
+            return redirect(url_for('views.student',id=student.id, user=current_user))
+        except sqlalchemy.exc.IntegrityError as ex:
+            db.session.rollback()
+            flash(f'Save Failed {type(ex)}', category='error')
+    return render_template("student_edit.html",student=student, user=current_user)
+
+
+@views.route('/student/<int:id>', methods=['GET','POST'])
+@login_required
+def student_edit(id):
+    student = Student.query.get_or_404(id)
+    if request.method == 'GET':
+        return render_template("student_edit.html", student=student, user=current_user)
+
+    # checkouts =  StudentInstrument.query.all().Join(Student)
+    # checkouts = Student.query.join(Student.instruments)
+    # checkouts = db.session.query(Student).join(Instrument,Student.instruments)
+    # checkouts = db.session.query.join(Instrument)
+    student.email = request.form.get('email')
+    student.first_name = request.form.get('first_name')
+    student.last_name = request.form.get('last_name')
+    db.session.commit()
+    flash('Student Updated!', category='success')
+    return redirect(url_for('views.student_edit',id=student.id, user=current_user))
+
+@views.route('/instrument/new', methods=['GET','POST'])
+@login_required
+def instrument_new():
+    instrument = Instrument()
+    if request.method == 'POST':
+        instrument.type = request.form['type']
+        # instrument.size = request.form['size']
+        instrument.brand = request.form.get('brand')
+        db.session.add(instrument)
+        db.session.commit()
+        flash('Instrument Added!', category='success')
+        return redirect(url_for('views.instrument',id=instrument.id, user=current_user))
+
+    return render_template("instrument_new.html",user=current_user)    
+
+# Checkouts
+@views.route('/checkout', methods=['GET','POST'])
+@login_required
+def checkout_list():
+    # checkouts = StudentInstrument.query.all()
+    # students = Student.query.join(StudentInstrument).join(Instrument).all()
+    students = Student.query(Student.id, Student.first_name).join(StudentInstrument).query(StudentInstrument.checkout_date).join(Instrument).query(Instrument.type).all()
+
+    return render_template("checkout_list.html",students=students, user=current_user)    
