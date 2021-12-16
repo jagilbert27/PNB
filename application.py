c5c58352ce5989d03e89ed06b5fd6b85a57d5b67
Jeff Gilbert <jeff.gilbert@usanad.com>
2021-12-15 19:13:43 -0500

    test

--- application.py
+++ application.py
@@ -1,6 +1,13 @@
-from website import create_app
+# from website import create_app
 
-app = create_app()
+# app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask
+application = Flask(__name__)
+app = application
+@application.route('/')
+def hello_world():
+        return 'howdy'
