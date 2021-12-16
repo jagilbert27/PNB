16f60ae0aedfd06a86971f1203c6b8616e444509
Jeff Gilbert <jeff.gilbert@usanad.com>
2021-12-16 07:46:09 -0500

    debugging

--- application.py
+++ application.py
@@ -1,13 +1,13 @@
-# from website import create_app
+from website import create_app
 
-# app = create_app()
+application = create_app()
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    application.run(debug=True)
 
-from flask import Flask
-application = Flask(__name__)
-app = application
-@application.route('/')
-def hello_world():
-        return 'howdy'
+# from flask import Flask
+# application = Flask(__name__)
+# app = application
+# @application.route('/')
+# def hello_world():
+#         return 'howdy'
